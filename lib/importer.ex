1587feb8a9b55270766346732460dac19b41512c
Cristopher Rodrigues <cristopher.rodrigues993@gmail.com>
2019-03-11 17:40:32 -0300

    init mix

---
+++ lib/importer.ex
@@ -0,0 +1,18 @@
+defmodule Importer do
+  @moduledoc """
+  Documentation for Importer.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Importer.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
